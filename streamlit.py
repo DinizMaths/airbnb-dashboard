79f7a5ba4fff8b2f2e8291b6977bcc291481f499
Matheus <dinizmaths@gmail.com>
2023-06-22 19:45:04 -0300

    update database

--- streamlit.py
+++ streamlit.py
@@ -3,6 +3,21 @@ import numpy     as np
 import pandas    as pd
 
 
-st.set_page_config(layout="wide")
+# DATA
+airbnb = pd.read_csv("data/airbnb.csv")
 
-airbnb = pd.read_csv("/data/airbnb.csv")
+
+# STREAMLIT PAGE
+st.set_page_config(layout="wide")
+st.title("Airbnb - New York")
+
+st.markdown("")
+st.markdown("O código para este dashboard está disponível em: ![GitHub](https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white)")
+st.markdown("If you are interested in how this app was developed check out my [Medium article](https://tim-denzler.medium.com/is-bayern-m%C3%BCnchen-the-laziest-team-in-the-german-bundesliga-770cfbd989c7)")
+    
+
+st.markdown("")
+see_data = st.expander("Clique para ver os dados 👉")
+
+with see_data:
+  st.dataframe(data=airbnb)
